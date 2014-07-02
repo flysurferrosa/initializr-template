185fdedf5be4021404cff1d269ceeccaa0465238
[flysurferrosa] <[ott.joel@gmail.com]>
2014-07-01 17:11:46 -0700

    made game function

---
+++ js/libs/cardGame.js
@@ -0,0 +1,102 @@
+
+  function Deck() {
+  this.cards = [];
+  this.count = function() {
+    return this.cards.length;
+  }
+  this.init = function() {
+    for (s =  1; s <= 4; s++) {
+      for (r = 1; r <= 13; r++) {
+        this.cards.push(new Card(r, s));
+        }   
+      }
+    }
+  }
+ 
+  function Card(rank, suit) {
+    this.rank = rank;
+    this.suit = suit;
+    this.show = function() {
+      console.log(this.rank + " of " + this.suit);
+    }
+  }
+
+  d = new Deck;
+  d.init();
+  d.count();
+
+  function Player() {
+    this.cards = [];
+    this.name = name;
+  }
+
+  p1 = new Player;
+  p2 = new Player;
+
+  function Hand() {
+    this.cards = [];
+  }
+
+  pile = new Hand;
+  p1Points = new Hand;
+  p2Points = new Hand;
+
+  var deal = function (aDeck, p1, p2) {
+    
+    while (aDeck.cards.length !== 0) {
+      var randomIndex1 = Math.floor(Math.random()*aDeck.cards.length);
+      var randomCard1 = aDeck.cards.splice(randomIndex1, 1)[0];
+      p1.cards.push(randomCard1);
+
+      var randomIndex2 = Math.floor(Math.random()*aDeck.cards.length);
+      var randomCard2 = aDeck.cards.splice(randomIndex2, 1)[0];
+      p2.cards.push(randomCard2);
+    }
+  }
+
+  deal(d, p1, p2);
+
+  var cardFromP1, cardFromP2; 
+
+  var playGame = function (cardFromP1, cardFromP2, p1, p2, pile, p2Points, p1Points) {
+
+       
+      cardFromP1 = p1.cards.pop();
+      console.log("Player 1 played a "+cardFromP1.rank+" of "+cardFromP1.suit);
+      pile.cards.push(cardFromP1);
+      
+      while (p1.cards.length != 0 && p2.cards.length != 0) {
+
+        cardFromP2 = p2.cards.pop();
+        console.log("Player 2 played a "+cardFromP2.rank+" of "+cardFromP2.suit);
+        pile.cards.push(cardFromP2);
+
+        if (cardFromP2["rank"] == pile.cards[pile.cards.length-2]["rank"]) {
+          console.log("Player 2 takes the pile!")
+          p2Points.cards = p2Points.cards.concat(p2.cards);
+        }
+
+        cardFromP1 = p1.cards.pop();
+        console.log("Player 1 played a "+cardFromP1.rank+" of "+cardFromP2.suit);
+        pile.cards.push(cardFromP1);
+
+        if (cardFromP1["rank"] == pile.cards[pile.cards.length-2]["rank"]) {
+          console.log("Player 1 takes the pile!")
+          p1Points.cards = p1Points.cards.concat(p1.cards);
+        }
+      }
+        if (p1Points.cards.length > p2Points.cards.length) {
+          console.log("Player 1 wins!")
+        }
+        else if (p1Points.cards.length < p2Points.cards.length) {
+          console.log("Player 2 wins!")
+        }
+        else {
+          console.log("It's a tie!")
+        }
+  }
+
+
+
+
+
